46253b8ea1ad01d2130e3752c34aee2a0b7aad39
qijieguan <qijieguan7@gmail.com>
2021-06-14 01:37:27 -0700

    signUp css changed

--- component/SignUp.js
+++ component/SignUp.js
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 24,
         fontWeight: 'bold',
-        color: 'black'
+        color: 'lightgreen'
     },
     inputView: {
         marginLeft: '5%',
